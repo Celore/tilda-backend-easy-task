94a906c6941450ae404f6a6a82cd5059aed4e385
Eugene Yushenkov <ieklers@gmail.com>
2023-02-05 21:45:46 +0300

    Task 3

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 
+use Yushenkov\Tilda\Arrays\IntMatrixConsolePrinter;
 use Yushenkov\Tilda\Arrays\IntMatrixGenerator;
 use Yushenkov\Tilda\Arrays\UniqueIntGenerator;
 use Yushenkov\Tilda\Stairs\StairsPrinter;
@@ -22,5 +23,13 @@ $intGenerator->setMinNumber(1)
 $matrixGenerator = new IntMatrixGenerator($intGenerator);
 $matrix = $matrixGenerator->generate(5, 7);
 
-$printer = new \Yushenkov\Tilda\Arrays\IntMatrixConsolePrinter();
+$printer = new IntMatrixConsolePrinter();
 $printer->print($matrix);
+
+/**
+ * Task 3
+ * Есть несколько вариантов как выполнить задачу, зависит от того как работает приложение.
+ * Если у нас React-приложение - бэк на ините пришлет значение для телефона и замену будет делать фронт.
+ * Если у нас server side rendering и мы не можем править шаблон (заменить DIGITS), то придется буферизировать
+ * HTML-шаблон, а затем регуляркой заменять DIGITS на числа в зависимости от определенного по IP города юзера
+ */
